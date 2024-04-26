623f982f82843883ea6e2fd890e3ffd869f5245d
YourDudeIsHere <166920933+YourDudeIsHere@users.noreply.github.com>
2024-04-26 16:12:53 +0100

    Menu Anim attempted
    I tried to make the menu but failed so im going to recreate it using multiple layers

--- ModiMark Version 1/Assets/Menu Assets/MenuButtons.cs
+++ ModiMark Version 1/Assets/Menu Assets/MenuButtons.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour
 {
-     void OnMouseOver()
+   
+    public Button Play;
+    public void PlayGame()
     {
-        Debug.Log("This is quit button");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    private void OnMouseExit()
+    {
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Play = GetComponent<Button>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      
     }
+ 
+   
 }
